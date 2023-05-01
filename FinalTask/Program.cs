dd41cab6a85a848b586cc41156dcb3d65bfa1ca9
Dmitry Pleshchev <dmitrydevlax@yandex.ru>
2023-05-01 12:39:30 +0500

    Function InputArrayElements added

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -3,4 +3,17 @@
 либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами.*/
 
+Console.WriteLine("Введите количество элементов массива: ");
+int length = int.Parse(Console.ReadLine());
+string[] stringArray1 = new string[length];
 
+void InputArrayElements(string[] stringArray1)
+{
+  for (int i = 0; i < stringArray1.Length; i++)
+  {
+    Console.WriteLine($"введите {i+1} элемент массива: ");
+    stringArray1[i] = Console.ReadLine();
+  }
+}
+
+InputArrayElements(stringArray1);
