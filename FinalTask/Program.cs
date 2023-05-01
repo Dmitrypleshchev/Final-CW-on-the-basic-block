9e1199a871a363c3eead9763447ab0b604d6a275
Dmitry Pleshchev <dmitrydevlax@yandex.ru>
2023-05-01 12:58:16 +0500

    Function CheckingArrayElement added

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -16,4 +16,25 @@ void InputArrayElements(string[] stringArray1)
   }
 }
 
+string[] CheckingArrayElements(string[] stringArray1)
+{
+  int count = 0;
+  for (int i = 0; i < stringArray1.Length; i++)
+  {
+    if (stringArray1[i].Length <=3)
+    count++;
+  }
+  string[] array2 = new string[count];
+  int newElements = 0;
+  for (int i = 0; i < stringArray1.Length; i++)
+  {
+    if (stringArray1[i].Length <=3)
+    {
+      array2[newElements] = stringArray1[i];
+      newElements++;
+    }
+  }
+  return array2;
+}
+
 InputArrayElements(stringArray1);
